3b08f3f64e6ca85a82d4a18ba82aba05a9170c97
Sushmita-Hubli <hublisushmita7@gmail.com>
2025-12-04 13:33:45 -0500

    Fix WebApplicationTests to use test profiole (H2 database) & updated ci.yml file

--- src/test/java/com/example/webapp/WebappApplicationTests.java
+++ src/test/java/com/example/webapp/WebappApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // ← Add this line!
 class WebappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test just checks if the Spring context loads successfully
+    }
 
 }
